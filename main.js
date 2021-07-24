ddb9015cbcdb75b63f482fb8734ce8a07c8e166b
faisalthedev <faisal13108@gmail.com>
2021-07-24 16:32:35 +0600

    Single meal page & ingredient list added

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const searchBtn = document.getElementById("searchBtn");
 const searchResult = document.getElementById("searchResult");
 const searchInput = document.getElementById("searchInput");
+const mealList = document.getElementById("mealList");
+const singleMeal = document.getElementById("singleMeal");
+const singleMealCard = document.getElementById("singleMealCard");
+
+// Hide default area
+singleMeal.style.display = "none";
+mealList.style.display = "block";
 
 searchBtn.addEventListener("click", (e) => {
 	e.preventDefault();
@@ -11,9 +18,10 @@ searchBtn.addEventListener("click", (e) => {
 			const meals = data.meals;
 			let mealUI = "";
 
-			meals.forEach((meal) => {
-				mealUI += `
-          <div class="col">
+			if (meals !== null) {
+				meals.forEach((meal) => {
+					mealUI += `
+          <div class="col" onclick="showSingleMeal('${meal.idMeal}')">
             <div class="card h-100 shadow text-center">
               <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}" />
               <div class="card-body">
@@ -22,8 +30,48 @@ searchBtn.addEventListener("click", (e) => {
             </div>
           </div>
         `;
-			});
+				});
+			} else {
+				mealUI = `<p class="text-center text-danger">No result found! Search again...</p>`;
+				searchInput.value = "";
+			}
 
 			searchResult.innerHTML = mealUI;
 		});
 });
+
+function showSingleMeal(mealId) {
+	// Show hide area
+	mealList.style.display = "none";
+	singleMeal.style.display = "block";
+
+	const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+	fetch(url)
+		.then((res) => res.json())
+		.then((mealData) => {
+			const singleMealInfo = mealData.meals[0];
+
+			let singleMealUI = `
+        <div class="card">
+          <img src="${singleMealInfo.strMealThumb}" class="card-img-top" alt="${singleMealInfo.strMeal}" />
+          <div class="card-body">
+            <h5 class="card-title">${singleMealInfo.strMeal}</h5>
+            <p class="card-text">
+              <ol id="ingredientList">
+                <li>${singleMealInfo.strIngredient1}</li>
+                <li>${singleMealInfo.strIngredient2}</li>
+                <li>${singleMealInfo.strIngredient3}</li>
+                <li>${singleMealInfo.strIngredient4}</li>
+                <li>${singleMealInfo.strIngredient5}</li>
+                <li>${singleMealInfo.strIngredient6}</li>
+                <li>${singleMealInfo.strIngredient7}</li>
+                <li>${singleMealInfo.strIngredient8}</li>
+              </ol>
+            </p>
+          </div>
+        </div>
+      `;
+
+			singleMealCard.innerHTML = singleMealUI;
+		});
+}
